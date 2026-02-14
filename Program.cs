0ef23ec78f71567a3e2e9877eb57b0024590ae63
zachschool1 <zacharyjordan@my.whccd.edu>
2026-02-14 15:11:51 -0800

    finish part of it

--- Program.cs
+++ Program.cs
@@ -4,9 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
 
+            IntroductionMsg();
+
+            Console.WriteLine("What is your name?");
+            string inputName = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            int inputAge = Convert.ToInt32(Console.ReadLine());
+
+            ShowInfo(inputName, inputAge);
+
+            double divisionAnswer = DoDivision(12, 4);
+            Console.WriteLine($"Your divison answer is {divisionAnswer}");
+           
 
         }
+
+        static void IntroductionMsg()
+        {
+            Console.WriteLine("Hello! Welcome to the Program Organizer!");
+        }
+
+        static void ShowInfo(string name, int age)
+        {
+            Console.WriteLine($"Hello {name}, Your age is {age}.");
+        }
+
+        static double DoDivision(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
     }
 }
